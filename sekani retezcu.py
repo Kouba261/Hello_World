c51aabafb4ae5bff4b2996aa1a0e6f4262207890
Kouba261 <36225532+Kouba261@users.noreply.github.com>
2018-02-08 18:19:32 +0100

    sekani retezcu

---
+++ sekani retezcu.py
@@ -0,0 +1,15 @@
+retezec = 'čokoláda'
+kousek = retezec[5:] # usekne slovo od 4. pismene
+print(kousek)
+
+
+retez1 = "Nikola"
+sek1 = retez1[:3] # vybere vše až po 4.písmeno
+
+print(sek1)
+
+retez2 = "Superman"
+sek2 = retez2[2:6] # vybere vse od 3. do 5. písmene
+print (sek2)
+
+
