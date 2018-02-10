6bda32e7f29e57b0abaa1b8298fcafee1142c484
Kouba261 <36225532+Kouba261@users.noreply.github.com>
2018-02-10 16:43:50 +0100

    Nahodne vylosuje kartu z balicku

---
+++ Losovani karty.py
@@ -0,0 +1,40 @@
+import random
+
+balicek = []
+znak = '♠', '♥', '♦', '♣'
+hodnota = list(range(2,11)) + ['J','Q','K','A'] #list.range vytvori radu cisel od 2 do 11
+karta = []
+     
+
+    
+def losovani(): #FUNKCE vylosuje nahodnou kartu
+    
+    for Vyber_znak in znak:
+        for Vyber_hodnota in hodnota:
+                    karta = str(Vyber_hodnota)+Vyber_znak  # str prevede na retezec
+                    balicek.append(karta)
+                    
+
+    random.shuffle(balicek) #nahodne zamicha balicek
+
+    Vyber_kartu = random.choice(balicek)
+
+    print("Vylosoval jsem kartu: ", Vyber_kartu)
+      
+
+def dalsi_karta(): #FUNKCE zepta se zda chces dalsi kartu
+        
+    Nova_karta = input("Chces losovat kartu?")
+
+    if Nova_karta == "ano":
+            losovani()
+    elif Nova_karta == "ne":
+        print ("Dobrá, nebudeme losovat")
+        return
+    else:
+        print("Nerozumím Ti, odpovídej 'ano' nebo 'ne'")
+        return
+    dalsi_karta()
+        
+dalsi_karta()
+
